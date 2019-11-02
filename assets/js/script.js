ad2aca7250267017ea1aacd38d57e5c8a3c71db7
Devin <alexanderdev117@gmail.com>
2019-11-02 13:04:03 -0500

    in class additions 11/2

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,6 @@ var a1;
 var a2;
 var a3;
 var a4;
-var name;
 
 //Opening layout.
 function quizopening() {
@@ -91,7 +90,7 @@ function wrongcw() {
             cw.textContent = "";
             cwtime = 2;
         }
-    }, 400);
+    }, 350);
 }
 
 //Question 1 layout.
@@ -190,24 +189,339 @@ function question3() {
     a2.remove();
     a3.remove();
     a4.remove();
+    question.textContent = "Which of the following is not considered a JavaScript operator?";
+       
+    a1 = document.createElement("BUTTON");
+    a1.innerHTML = "new";
+    document.body.children[4].appendChild(a1);
+    a1.addEventListener("click", function () {
+        wrongcw();
+        question4();
+        wrong();
+    });
+ 
+    a2 = document.createElement("BUTTON");
+    a2.innerHTML = "this";
+    document.body.children[4].appendChild(a2);
+    a2.addEventListener("click", function () {
+        correctcw();
+        question4();
+        correct();
+    });
+
+    a3 = document.createElement("BUTTON");
+    a3.innerHTML = "delete";
+    document.body.children[4].appendChild(a3);
+    a3.addEventListener("click", function () {
+        wrongcw();
+        question4();
+        wrong();
+    });
+
+    a4 = document.createElement("BUTTON");
+    a4.innerHTML = "typeof";
+    document.body.children[4].appendChild(a4);
+    a4.addEventListener("click", function () {
+        wrongcw();
+        question4();
+        wrong();
+    });
+}
+
+//Question 4 layout.
+function question4() {
+    a1.remove();
+    a2.remove();
+    a3.remove();
+    a4.remove();
+    question.textContent = "JavaScript is interpreted by ____";
+       
+    a1 = document.createElement("BUTTON");
+    a1.innerHTML = "Client";
+    document.body.children[4].appendChild(a1);
+    a1.addEventListener("click", function () {
+        correctcw();
+        question5();
+        correct();
+    });
+ 
+    a2 = document.createElement("BUTTON");
+    a2.innerHTML = "Server";
+    document.body.children[4].appendChild(a2);
+    a2.addEventListener("click", function () {
+        wrongcw();
+        question5();
+        wrong();
+    });
+
+    a3 = document.createElement("BUTTON");
+    a3.innerHTML = "Oject";
+    document.body.children[4].appendChild(a3);
+    a3.addEventListener("click", function () {
+        wrongcw();
+        question5();
+        wrong();
+    });
+
+    a4 = document.createElement("BUTTON");
+    a4.innerHTML = "None of these";
+    document.body.children[4].appendChild(a4);
+    a4.addEventListener("click", function () {
+        wrongcw();
+        question5();
+        wrong();
+    });
+}
+
+//Question 5 layout.
+function question5() {
+    a1.remove();
+    a2.remove();
+    a3.remove();
+    a4.remove();
+    question.textContent = "____method evaluates a string of JavaScript code in the context of the specified object.";
+       
+    a1 = document.createElement("BUTTON");
+    a1.innerHTML = "Evparse";
+    document.body.children[4].appendChild(a1);
+    a1.addEventListener("click", function () {
+        wrongcw();
+        question6();
+        wrong();
+    });
+ 
+    a2 = document.createElement("BUTTON");
+    a2.innerHTML = "Parsclick";
+    document.body.children[4].appendChild(a2);
+    a2.addEventListener("click", function () {
+        wrongcw();
+        question6();
+        wrong();
+    });
+
+    a3 = document.createElement("BUTTON");
+    a3.innerHTML = "ParseInt";
+    document.body.children[4].appendChild(a3);
+    a3.addEventListener("click", function () {
+        wrongcw();
+        question6();
+        wrong();
+    });
+
+    a4 = document.createElement("BUTTON");
+    a4.innerHTML = "Eval";
+    document.body.children[4].appendChild(a4);
+    a4.addEventListener("click", function () {
+        correctcw();
+        question6();
+        correct();
+    });
+}
+
+//Question 6 layout.
+function question6() {
+    a1.remove();
+    a2.remove();
+    a3.remove();
+    a4.remove();
+    question.textContent = "The C in CSS stands for: ";
+       
+    a1 = document.createElement("BUTTON");
+    a1.innerHTML = "Cursive";
+    document.body.children[4].appendChild(a1);
+    a1.addEventListener("click", function () {
+        wrongcw();
+        question7()
+        wrong();
+    });
+ 
+    a2 = document.createElement("BUTTON");
+    a2.innerHTML = "Coded";
+    document.body.children[4].appendChild(a2);
+    a2.addEventListener("click", function () {
+        wrongcw();
+        question7()
+        wrong();
+    });
+
+    a3 = document.createElement("BUTTON");
+    a3.innerHTML = "Cascading";
+    document.body.children[4].appendChild(a3);
+    a3.addEventListener("click", function () {
+        correctcw();
+        question7();
+        correct();
+    });
+
+    a4 = document.createElement("BUTTON");
+    a4.innerHTML = "Coding";
+    document.body.children[4].appendChild(a4);
+    a4.addEventListener("click", function () {
+        wrongcw();
+        question7()
+        wrong();
+    });
+}
+
+//Question 7 layout.
+function question7() {
+    a1.remove();
+    a2.remove();
+    a3.remove();
+    a4.remove();
+    question.textContent = "HTML is in charge of the ___";
+       
+    a1 = document.createElement("BUTTON");
+    a1.innerHTML = "Content";
+    document.body.children[4].appendChild(a1);
+    a1.addEventListener("click", function () {
+        correctcw();
+        question8();
+        correct();
+    });
+ 
+    a2 = document.createElement("BUTTON");
+    a2.innerHTML = "Colors";
+    document.body.children[4].appendChild(a2);
+    a2.addEventListener("click", function () {
+        wrongcw();
+        question8()
+        wrong();
+    });
+
+    a3 = document.createElement("BUTTON");
+    a3.innerHTML = "Size";
+    document.body.children[4].appendChild(a3);
+    a3.addEventListener("click", function () {
+        wrongcw();
+        question8()
+        wrong();
+    });
+
+    a4 = document.createElement("BUTTON");
+    a4.innerHTML = "Function";
+    document.body.children[4].appendChild(a4);
+    a4.addEventListener("click", function () {
+        wrongcw();
+        question8()
+        wrong();
+    });
+}
+
+//Question 8 layout.
+function question8() {
+    a1.remove();
+    a2.remove();
+    a3.remove();
+    a4.remove();
+    question.textContent = "If . represents class, # represents ___";
+       
+    a1 = document.createElement("BUTTON");
+    a1.innerHTML = "Number";
+    document.body.children[4].appendChild(a1);
+    a1.addEventListener("click", function () {
+        wrongcw();
+        question9()
+        wrong();
+    });
+ 
+    a2 = document.createElement("BUTTON");
+    a2.innerHTML = "Id";
+    document.body.children[4].appendChild(a2);
+    a2.addEventListener("click", function () {
+        correctcw();
+        question9();
+        correct();
+    });
+
+    a3 = document.createElement("BUTTON");
+    a3.innerHTML = "Pound";
+    document.body.children[4].appendChild(a3);
+    a3.addEventListener("click", function () {
+        wrongcw();
+        question9()
+        wrong();
+    });
+
+    a4 = document.createElement("BUTTON");
+    a4.innerHTML = "Hash";
+    document.body.children[4].appendChild(a4);
+    a4.addEventListener("click", function () {
+        wrongcw();
+        question9()
+        wrong();
+    });
+}
+
+//Question 9 layout.
+function question9() {
+    a1.remove();
+    a2.remove();
+    a3.remove();
+    a4.remove();
+    question.textContent = "Which of the following is NOT a loop?";
+       
+    a1 = document.createElement("BUTTON");
+    a1.innerHTML = "for";
+    document.body.children[4].appendChild(a1);
+    a1.addEventListener("click", function () {
+        wrongcw();
+        question10()
+        wrong();
+    });
+ 
+    a2 = document.createElement("BUTTON");
+    a2.innerHTML = "while";
+    document.body.children[4].appendChild(a2);
+    a2.addEventListener("click", function () {
+        wrongcw();
+        question10()
+        wrong();
+    });
+
+    a3 = document.createElement("BUTTON");
+    a3.innerHTML = "do-while";
+    document.body.children[4].appendChild(a3);
+    a3.addEventListener("click", function () {
+        wrongcw();
+        question10()
+        wrong();
+    });
+
+    a4 = document.createElement("BUTTON");
+    a4.innerHTML = "don't";
+    document.body.children[4].appendChild(a4);
+    a4.addEventListener("click", function () {
+        correctcw();
+        question10();
+        correct();
+    });
+}
+
+//Question 10 layout.
+function question10() {
+    a1.remove();
+    a2.remove();
+    a3.remove();
+    a4.remove();
     question.textContent = "Inside which HTML element (< >) do we put the JavaScript?";
        
     a1 = document.createElement("BUTTON");
     a1.innerHTML = "js";
     document.body.children[4].appendChild(a1);
     a1.addEventListener("click", function () {
+        wrong();
         wrongcw();
         postquiz();
-        wrong(); 
     });
  
     a2 = document.createElement("BUTTON");
     a2.innerHTML = "scripting";
     document.body.children[4].appendChild(a2);
     a2.addEventListener("click", function () {
+        wrong();
         wrongcw();
-        postquiz();
-        wrong(); 
+        postquiz(); 
     });
 
     a3 = document.createElement("BUTTON");
@@ -223,16 +537,16 @@ function question3() {
     a4.innerHTML = "javascript";
     document.body.children[4].appendChild(a4);
     a4.addEventListener("click", function () {
+        wrong();
         wrongcw();
         postquiz();
-        wrong();
     });
 }
 
 //Post-quiz layout.
 function postquiz() {
     timepoints();
-    clearInterval (timerInterval); 
+    seconds = 0; 
     a1.remove();
     a2.remove();
     a3.remove();
@@ -244,8 +558,8 @@ function postquiz() {
     a1.innerHTML = "Submit";
     document.body.children[4].appendChild(a1);
     a1.addEventListener("click", function () {
-        name = highscore.value;
-        localStorage.setItem(name, score);
+        localStorage.setItem("user", highscore.value);
+        localStorage.setItem("score", score);
         scorepage();
     });
 }
@@ -258,10 +572,8 @@ function scorepage() {
     highscore.style.visibility = "hidden";
     banner.textContent = "High Scores"
     
-    localStorage.getItem(name, score);
-    var a = JSON.stringify(localStorage);
-    question.textContent = a;
-
+    question.textContent = localStorage.getItem("user") + ": " + localStorage.getItem("score");
+    
     a1 = document.createElement("BUTTON");
     a1.innerHTML = "Restart Quiz";
     document.body.children[4].appendChild(a1);
